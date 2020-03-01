c519103be138fcaf4795535827873170597f0b93
Andre Kristanto <akristan001@e.ntu.edu.sg>
2020-03-01 15:47:38 +0800

    Use mapToString for table data

--- src/routes/Home/GraphTableSection/parts.jsx
+++ src/routes/Home/GraphTableSection/parts.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 // Components
 import NodeLinkActions from '../../../components/NodeLinkActions';
 // Utils
+import { mapToString } from '../../../utils/objects';
 import { getLinkSource, getLinkTarget } from '../../../utils/graph';
 
 export const nodeToGraphTableData = (focusOn, edit, remove) => obj => ({
-  ...obj,
+  ...mapToString(obj),
   Actions: (
     <NodeLinkActions focusOn={focusOn} edit={edit} remove={remove} data={obj} />
   ),
 });
 
 export const linkToGraphTableData = (focusOn, edit, remove) => obj => ({
-  ...obj,
+  ...mapToString(obj),
   source: getLinkSource(obj),
   target: getLinkTarget(obj),
   Actions: (
