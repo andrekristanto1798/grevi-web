cb2228cd12868e72f78d3dd8e1650a37aed16284
Andre Kristanto <akristan001@e.ntu.edu.sg>
2020-03-10 10:00:57 +0800

    Update the newest datasets
    - Rename cluster_graph to Les Miserables

--- src/data/graphs/index.js
+++ src/data/graphs/index.js
@@ -1,23 +1,29 @@
-const sampleGraph = require('./sample_graph.json');
-const clusterGraph = require('./cluster_graph.json');
 const covid19sg = require('./covid_19_sg.json');
+const lesMiserables = require('./Les Miserables.json');
+const wordAdjancencies = require('./Word Adjacencies.json');
+const chicago = require('./Chicago.json');
 const d3Deps = require('./d3_deps.json');
 
 export const graphFileList = [
   {
-    name: 'Sample Graph.json',
-    graph: sampleGraph,
-    desc: 'It is good to play around',
-  },
-  {
-    name: 'Sample Clustered Graph.json',
-    graph: clusterGraph,
-    desc: 'It has clustered graphs',
-  },
-  {
     name: 'COVID-19 Singapore.json',
     graph: covid19sg,
     desc: 'COVID-19 Cluster in Singapore',
+  },
+  {
+    name: 'Les Miserables.json',
+    graph: lesMiserables,
+    desc: `This undirected network contains co-occurances of characters in Victor Hugo's novel 'Les Misérables'`,
+  },
+  {
+    name: 'Word Adjacencies.json',
+    graph: wordAdjancencies,
+    desc: `This is the undirected network of common noun and adjective adjacencies for the novel "David Copperfield" by English 19th century writer Charles Dickens`,
+  },
+  {
+    name: 'Chicago Graph.json',
+    graph: chicago,
+    desc: `This is the road transportation network of the Chicago region (USA)`,
   },
   {
     name: 'D3 Dependencies.json',
